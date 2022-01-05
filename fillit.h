594b8db21c2d59bef0e11a0b7ee6e67a5769616d
Ilona Shakurova <leverseau19@gmail.com>
2022-01-05 17:40:51 +0200

    Add new type to header

--- fillit.h
+++ fillit.h
@@ -6,12 +6,19 @@
 /*   By: ishakuro <ishakuro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 14:30:08 by ishakuro          #+#    #+#             */
-/*   Updated: 2022/01/05 14:44:17 by ishakuro         ###   ########.fr       */
+/*   Updated: 2022/01/05 17:16:31 by ishakuro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # ifndef FILLIT_H
 #define FILLIT_H
+
+# include "libft/libft.h"
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+typedef char **string_array;
 
 typedef struct	s_piece
 {
